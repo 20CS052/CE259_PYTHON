262f8b282ef9e761d7f057e34f1c6239d6785fdd
20CS052 <91415483+20CS052@users.noreply.github.com>
2022-01-15 21:39:28 +0530

    Create Tuple_d.py
    Practical_2 :
    Tuple  :
    d. :

---
+++ practical_2/Tuple_d.py
@@ -0,0 +1,8 @@
+# Practical AIM             :-  d. Write a Python program to convert a tuple to a string.
+# Student-ID                :-  20CS052
+# Student-Name              :-  KISHANKUMAR PATEL
+# GitHub repository link    :-  https://github.com/20CS052/CE259_PYTHON.git
+
+tuple7 = ('K', 'I', 'S', 'H', 'A', 'N')
+str1 = ''.join(tuple7)  #convert a tuple to a string using .join()
+print(str1)
