c6651bb9182fa71801bb84bb2c119c16f70811be
20CS052 <91415483+20CS052@users.noreply.github.com>
2022-01-15 21:58:26 +0530

    Create Set_c.py
    Practical_2 :
    Set  :
    c. :

---
+++ practical_2/Set_c.py
@@ -0,0 +1,17 @@
+# Practical AIM             :-  c. Write a Python program to create an intersection, Union, difference of sets.
+# Student-ID                :-  20CS052
+# Student-Name              :-  KISHANKUMAR PATEL
+# GitHub repository link    :-  https://github.com/20CS052/CE259_PYTHON.git
+
+# sets are define
+A = {21, 31, 43, 62, 82, 32, 46};
+B = {89, 32, 46, 23, 31, 44, 53};
+
+# intersection
+print("Intersection :", A & B)
+
+# union
+print("Union :", A | B)
+
+# difference
+print("Difference :", A - B)
