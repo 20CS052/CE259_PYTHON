59c58e738fc6567ea1979028d5691edd1c67ca4e
20CS052 <91415483+20CS052@users.noreply.github.com>
2022-01-15 21:39:15 +0530

    Create Tuple_b.py
    Practical_2 :
    Tuple  :
    b. :

---
+++ practical_2/Tuple_b.py
@@ -0,0 +1,8 @@
+# Practical AIM             :-  b. Write a Python program to create a tuple with numbers and print one item.
+# Student-ID                :-  20CS052
+# Student-Name              :-  KISHANKUMAR PATEL
+# GitHub repository link    :-  https://github.com/20CS052/CE259_PYTHON.git
+
+# #Creating a tuple with numbers
+tuple2 = 5, 10, 15, 20, 25
+print(tuple2[0])            #printing one item from tuple2
