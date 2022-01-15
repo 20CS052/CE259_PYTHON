4e7188cffc669fa536358c16ee64063021b3ef1b
20CS052 <91415483+20CS052@users.noreply.github.com>
2022-01-15 21:58:33 +0530

    Create Set_d.py
    Practical_2 :
    Set  :
    d. :

---
+++ practical_2/Set_d.py
@@ -0,0 +1,13 @@
+# Practical AIM             :-  d. Write a Python program to find maximum and the minimum value in a set.
+# Student-ID                :-  20CS052
+# Student-Name              :-  KISHANKUMAR PATEL
+# GitHub repository link    :-  https://github.com/20CS052/CE259_PYTHON.git
+
+#Create a set
+set_ex = {522, 103, 32, 1245, 2, 230}
+print("Original set elements:")
+print(set_ex)
+print("\nMaximum value of the given set:")
+print(max(set_ex))                              #print maximum value of given set
+print("\nMinimum value of the given set:")
+print(min(set_ex))                              #print minimum value of given set
