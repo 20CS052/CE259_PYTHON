6fc49035b49aa369864d98979ff3bc386647ba08
20CS052 <91415483+20CS052@users.noreply.github.com>
2022-01-25 10:55:26 +0530

    Create practical_5.py
    Practical_5 : You are given a string and your task is to swap cases.
    Sample Input: HackerRank.com presents "Pythonist 2".
    Sample Output: hACKERrANK.COM PRESENTS "pYTHONIST 2".

---
+++ practical_5.py
@@ -0,0 +1,9 @@
+# Practical AIM             :-  You are given a string and your task is to swap cases. 
+# Student-ID                :-  20CS052
+# Student-Name              :-  KISHANKUMAR PATEL
+# GitHub repository link    :-  https://github.com/20CS052/CE259_PYTHON.git
+
+
+Input_String = input()  #taking input as string
+#convert all lowercase letters to uppercase letters and vice versa. using .swapcase()
+print(Input_String.swapcase())
